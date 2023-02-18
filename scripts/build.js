b2a2e8fda0e626ecc1271a4c26f8fe4e1a4124a7
Jamie_ <59361355+JamieGrimwood@users.noreply.github.com>
2023-02-18 23:02:28 +0000

    obfuscation was dumb and doesn't change anything. Need to figure out a way to fix it.

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const minifyHtml = require('html-minifier').minify;
 const { minify } = require("terser");
-const JavaScriptObfuscator = require('javascript-obfuscator');
 
 const pagesDir = path.join(__dirname, '../pages');
 
@@ -28,8 +27,6 @@ fs.readdir(pagesDir, async (err, files) => {
     const outputFilePath = path.join(__dirname, '../static/build.js');
     const indexFile = fs.readFileSync(path.join(__dirname, '../src/index.js'), 'utf8');
     let output = `const pages = ${JSON.stringify(pages)};\n\n${indexFile}`;
-    output = JavaScriptObfuscator.obfuscate(output).getObfuscatedCode();
-    // We obfuscate the code so that the user can't just do router.navigate and access any page they wish.
     output = await minify(output);
     fs.writeFileSync(outputFilePath, output.code, 'utf8');
 
